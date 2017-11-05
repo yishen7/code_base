4fc3eefd7996ac0c93dafbb847b0622c83cc4dd4
qi <yishen18747@163.com>
2017-11-05 13:56:21 +0800

    update

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -1,4 +1,4 @@
-from ..models import Post, Category
+from ..models import Post, Category, Tag
 from django import template
 from django.db.models.aggregates import Count
 
@@ -23,3 +23,8 @@ def get_categories():
     # 记得在顶部引入 Count 函数
     # Count 计算分类下的文章数，其接受的参数为需要计数的模型的名称
     return Category.objects.annotate(num_posts=Count('post')).filter(num_posts__gt=0)
+
+@register.simple_tag
+def get_tags():
+    # 记得再顶部引入 Tag model
+    return Tag.objects.annotate(num_posts=Count('post')).filter(num_posts__gt=0)
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Post
 import markdown
-from .models import Post, Category
+from .models import Post, Category, Tag
 from comments.forms import CommentForm
 from django.views.generic import ListView, DetailView
 
@@ -196,12 +195,23 @@ class PostDetailView(DetailView):
         })
         return context
 
-class ArchivesView(IndexView):
+class ArchivesView(ListView):
     # 注意这里 created_time 是 Python 的 date 对象，其有一个 year 和 month 属性,
     # Python 中类实例调用属性的方法通常是 created_time.year，但是由于这里作为函数的参数列表，
     # 所以 Django 要求我们把点替换成了两个下划线，即 created_time__year。
+    model = Post
+    template_name = 'blog/index.html'
+    context_object_name = 'post_list'
     def get_queryset(self):
         year = self.kwargs.get('year')
         month = self.kwargs.get('month')
-        return super(ArchivesView, self).get_queryset().filter(create_time__year=year, create_time__month=month)
+        return super(ArchivesView, self).get_queryset().filter(created_time__year=year, created_time__month=month)
 
+class TagView(ListView):
+    model = Post
+    template_name = 'blog/index.html'
+    context_object_name = 'post_list'
+
+    def get_queryset(self):
+        tag = get_object_or_404(Tag, pk=self.kwargs.get('pk'))
+        return super(TagView, self).get_queryset().filter(tags=tag)
